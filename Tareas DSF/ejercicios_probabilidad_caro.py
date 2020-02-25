7fe2658d0395bdfcf1eeb9abf43eb464fb14ce5b
caro_lara <caro.lara@me.com>
2020-02-24 19:37:15 -0500

    Create ejercicios_probabilidad_caro.py

---
+++ Tareas DSF/ejercicios_probabilidad_caro.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""Ejercicios Probabilidad.ipynb
+
+
+In an English deck of cards there are 4 pints (hearts, spades, diamonds and clubs) and 13 cards per pint. That is, in total there are 52 cards. Suppose you choose two random cards. What is the probability that one of them is black since the other is also black?
+1.	1/2
+2.	25/52
+3.	25/51
+4.	26/51
+5.	None of above
+"""
+
+(((25/51)*(26/51))/(26/51))
+
+
+"""The probability that a Titi monkey likes to eat peanuts is 40%, and the probability of consuming it regularly is 20%. In addition, studies have shown that, among the Titi monkeys that eat peanuts, the probability of dying before 5 years is 5%. What is the probability of dying before 5 years of 5%. What is the probability that a Titi monkey eats peanuts regularly and dies before age 5?
+1.	0.1
+2.	0.04
+3.	0.02
+4.	0.01
+"""
+
+
+
+"""If you throw a 6-sided side 3 times. X1, X2 and X3 denote the value of the first, second and third throw.
+What is P (X1 + X2 + X3 <= 5)
+1.	10/36
+2.	5/36
+3.	10/216
+4.	5/216
+"""
+
+
+10 / 6 ** 3
+
+"""In an English deck of cards there are 4 pints (hearts, spades, diamonds and tremolo) and 13 cards per pints. That is, in total there are 52 cards. Suppose you choose two random cards. What is the probability that one of them is black since the other is red?
+1.	1/2
+2.	25/52
+3.	1/4
+4.	26/51
+"""
+25/51
+
+"""Cosider a fair coin and fair die. The possible outcome toss is "head" or "tail", while the possible outcome for the die throw is 1,2,3,4,5 or 6. You toss the coin 4 times and throw the die 2 times.
+The number of possible outcomes for this experiment is________
+"""
+
+ answer = 2 * 2 * 2 * 2 * 6 * 6
+
+
+"""A dart is thrown at circular dashboard such that it will land randomly over the area of the dashboard. What is the probability that it lands closer to the center than to the edge? Assume the dart always reaches the dashboard.
+1.	25%
+2.	50%
+3.	10%
+4.	33%
+"""
+
+
+
+answer = (1/4)*100
+
